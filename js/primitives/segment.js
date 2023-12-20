d1e09c985c4e20cf99250d3727788f73f5e9a1fa
crazz-zaac <r.alpha919@gmail.com>
2023-12-20 22:38:33 +0100

    Update: setting properties of segments connecting the points

--- js/primitives/segment.js
+++ js/primitives/segment.js
@@ -12,10 +12,10 @@ class Segment{
         return this.p1.equals(point) || this.p2.equals(point);
     }
 
-    draw(ctx, width = 2, color = "black"){
+    draw(ctx, width = 2, color = "black"){ 
         ctx.beginPath();
         ctx.lineWidth = width;
-        ctx.StrokeStyle = color;
+        ctx.strokeStyle = color;
         ctx.moveTo(this.p1.x, this.p1.y);
         ctx.lineTo(this.p2.x, this.p2.y);
         ctx.stroke();
